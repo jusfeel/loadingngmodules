174d8981da60828ae7947467bc9ad7d26f485a5f
NP User <npuser@np.io>
2017-10-28 05:51:57 +0000

    example

---
+++ Angular4PreLoadModules/src/app/app-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
+import { MarketsComponent } from './base/markets.component';
+import { SportsComponent } from './base/sports.component';
+
+const appRoutes: Routes = [
+  { path: '', redirectTo: '/markets', pathMatch: 'full' },
+  { path: 'markets', component: MarketsComponent },
+  { path: 'sports', component: SportsComponent },
+  { path: 'weather', loadChildren: './weather/weather.module#WeatherModule' },
+  { path: 'currency', loadChildren: './currency/currency.module#CurrencyModule' } 
+
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(appRoutes, {preloadingStrategy: PreloadAllModules})
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {
+
+}
